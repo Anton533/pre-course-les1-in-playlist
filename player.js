3c89a1a8ba165acd154f66ee4bf93ffafe89e389
Anton <anton.omelianenko533@gmail.com>
2023-12-15 18:16:36 +0200

    refactor: change project structure

--- player.js
+++ player.js
@@ -1,196 +1,128 @@
-const playlistOne = {
-  title: "Hip-Hop Hits",
-  coverImageUrl: "./img/hip-hop-hits.jpg",
-  info: {
-    totalTracksCount: 4,
-    totalTracksDurationInSeconds: 733,
-  },
-  label: "./icons/isHot.png",
-  tracks: [
-    {
-      coverImageUrl: "./img/eminem.png",
-      artistName: "Eminem",
-      title: "Rap God",
-      fileUrl: "./audio/eminem-rap-god.mp3",
-      isHot: true,
-    },
-    {
-      coverImageUrl: "./img/50cent.png",
-      artistName: "50cent",
-      title: "In da Club",
-      fileUrl: "./audio/50сent-in-da-club.mp3",
-      isHot: false,
-    },
-    {
-      coverImageUrl: "./img/dmx.png",
-      artistName: "DMX",
-      title: "X Gon' Give It To Ya",
-      fileUrl: "./audio/dmx-x-gon-give-it-to-ya.mp3",
-      isHot: true,
-    },
-    {
-      coverImageUrl: "./img/eminem50cent.png",
-      artistName: "Eminem feat 50 Cent, Cashis, Lloyd Banks",
-      title: "You Don't Know",
-      fileUrl: "./audio/eminem-50cent-you-dont-know.mp3",
-      isHot: true,
-    },
-  ],
-};
+import { playlists } from "./src/components/data.module.js";
 
-const playlistTwo = {
-  title: "Rap Hits 1990s",
-  coverImageUrl: "./img/Rap-Hits-1990s.png",
-  info: {
-    totalTracksCount: 4,
-    totalTracksDurationInSeconds: 942,
-  },
-  label: "./src/isHot.png",
-  tracks: [
-    {
-      coverImageUrl: "./img/public-enemy.png",
-      artistName: "Public Enemy",
-      title: "Fight the Power",
-      fileUrl: "./audio/public-enemy-fight-the-power.mp3",
-      isHot: true,
-    },
-    {
-      coverImageUrl: "./img/vanila-ice.png",
-      artistName: "Vanila Ice",
-      title: "Ice Ice Baby",
-      fileUrl: "./audio/vanilla-ice-ice-ice-baby.mp3",
-      isHot: true,
-    },
-    {
-      coverImageUrl: "./img/mc-hammer.png",
-      artistName: "MC Hammer",
-      title: "You Can’t Touch This",
-      fileUrl: "./audio/mc-hammer-u-can't-touch-this.mp3",
-      isHot: false,
-    },
-    {
-      coverImageUrl: "./img/public-enemy.png",
-      artistName: "Brand Nubian",
-      title: "Brand Nubian",
-      fileUrl: "./audio/brand-nubian-brand-nubian.mp3",
-      isHot: false,
-    },
-  ],
-};
-const playlist1 = document.getElementById("playlist-1");
-const playlist2 = document.getElementById("playlist-2");
+const playlistWrapper = document.getElementById("playlists-wrapper");
 
-renderPlaylist(playlistOne, playlist1);
-renderPlaylist(playlistTwo, playlist2);
+playlists.forEach((playlist) => renderPlaylist(playlist, playlistWrapper));
 
 function renderPlaylist(playlistForRendering, ele) {
-  createPlaylistHeading(playlistForRendering, ele);
+  const playlistEle = createEle("div", "playlist");
+  const tracksListEle = createEle("ul", "tracks-list");
+  ele.append(playlistEle);
+
+  createPlaylistHeading(playlistForRendering, playlistEle);
+  playlistEle.append(tracksListEle);
 
   playlistForRendering.tracks.forEach((track) => {
-    renderTrack(track, ele);
+    renderTrack(track, tracksListEle);
   });
 }
 
 function createEle(tagName, className) {
-  tag = document.createElement(tagName);
+  const tag = document.createElement(tagName);
   tag.classList = className;
   return tag;
 }
 
 function createPlaylistHeading(playlistForRendering, ele) {
-  const div = createEle("div", "playlist__heading");
-  createCover(playlistForRendering, div);
-  createInfo(playlistForRendering, div);
-  ele.append(div);
+  const playlistHeadingEle = createEle("div", "playlist__heading");
+  createCover(playlistForRendering, playlistHeadingEle);
+  createInfo(playlistForRendering, playlistHeadingEle);
+  ele.append(playlistHeadingEle);
 }
 
 function createCover(data, ele) {
   const div = createEle("div", "playlist__cover-wrapper");
-  const playlistImageElement = createEle("img", "playlist-cover");
-  playlistImageElement.src = data.coverImageUrl;
-  div.append(playlistImageElement);
+  const playlistImageEle = createEle("img", "playlist-cover");
+  playlistImageEle.src = data.coverImageUrl;
+  div.append(playlistImageEle);
   ele.append(div);
 }
 
 function createInfo(data, ele) {
-  const div = createEle("div", "playlist__info-wrapper");
-  createSubtitle(div);
-  createTitle(data, div);
-  createCountDuration(data, div);
-  createArtistsInfo(data, div);
-  ele.append(div);
+  const playlistInfoEle = createEle("div", "playlist__info-wrapper");
+  createSubtitle(playlistInfoEle);
+  createTitle(data, playlistInfoEle);
+  createCountDuration(data, playlistInfoEle);
+  createArtistsInfo(data, playlistInfoEle);
+  ele.append(playlistInfoEle);
 }
 
 function createSubtitle(ele) {
-  const div = createEle("div", "playlist__subtitle-wrapper");
-  const span = createEle("span", "playlist__subtitle");
-  span.innerHTML = `Playlist`;
-  div.append(span);
-  ele.append(div);
+  const playlistSubtitleWrapperEle = createEle(
+    "div",
+    "playlist__subtitle-wrapper"
+  );
+  const playlistSubtitleEle = createEle("span", "playlist__subtitle");
+  playlistSubtitleEle.innerHTML = `Playlist`;
+  playlistSubtitleWrapperEle.append(playlistSubtitleEle);
+  ele.append(playlistSubtitleWrapperEle);
 }
 
 function createTitle(data, ele) {
-  const playlistTitleElement = createEle("h2", "playlist__title");
-  playlistTitleElement.append(data.title);
-  ele.append(playlistTitleElement);
+  const playlistTitleEle = createEle("h2", "playlist__title");
+  playlistTitleEle.append(data.title);
+  ele.append(playlistTitleEle);
 }
 
 function createCountDuration(data, ele) {
-  const div = createEle("div", "playlist__count-duration-wrapper");
-  createCount(data, div);
-  createDuration(data, div);
-  ele.append(div);
+  const playlistCountDurationWrapperEle = createEle(
+    "div",
+    "playlist__count-duration-wrapper"
+  );
+  createCount(data, playlistCountDurationWrapperEle);
+  createDuration(data, playlistCountDurationWrapperEle);
+  ele.append(playlistCountDurationWrapperEle);
 }
 
 function createCount(data, ele) {
-  const span = createEle("span", "playlist__count");
-  span.append(`${data.info.totalTracksCount} tracks, `);
-  ele.append(span);
+  const playlistCountEle = createEle("span", "playlist__count");
+  playlistCountEle.append(`${data.info.totalTracksCount} tracks, `);
+  ele.append(playlistCountEle);
 }
 function createDuration(data, ele) {
-  const span = createEle("span", "playlist__duration");
+  const playlistDurationEle = createEle("span", "playlist__duration");
   const min =
     (data.info.totalTracksDurationInSeconds -
       (data.info.totalTracksDurationInSeconds % 60)) /
     60;
   const sec = data.info.totalTracksDurationInSeconds % 60;
-  span.append(`${min}m ${sec}s`);
-  ele.append(span);
+  playlistDurationEle.append(`${min}m ${sec}s`);
+  ele.append(playlistDurationEle);
 }
 
 function createArtistsInfo(data, ele) {
   const arr = data.tracks.slice(0, 3);
-  const artistsWrapper = createEle("span", "playlist__artists");
+  const playlistArtistsWrapperEle = createEle("span", "playlist__artists");
   const artists = arr.map((ele) => " " + ele.artistName);
   const andOther = createEle("span", "playlist__and-other");
   andOther.append(" " + "and others");
-  artistsWrapper.append(artists, andOther);
-  ele.append(artistsWrapper);
+  playlistArtistsWrapperEle.append(artists, andOther);
+  ele.append(playlistArtistsWrapperEle);
 }
 
 function renderTrack(data, ele) {
-  const trackEle = createEle("div", "track");
-  const trackEleImage = createEle("img", "track__cover");
-  const trackInfo = createEle("div", "track__info");
-  const trackTitle = createEle("div", "track__title");
-  const trackArtistName = createEle("span", "track__artist-name");
-  const trackName = createEle("span", "track__name");
-  const trackAudio = createEle("audio", "track__audio");
-  const isHot = createEle("img", "track__is-hot");
+  const trackEle = createEle("li", "track");
+  const trackImageEle = createEle("img", "track__cover");
+  const trackInfoEle = createEle("div", "track__info");
+  const trackTitleEle = createEle("span", "track__title");
+  const trackArtistNameEle = createEle("span", "track__artist-name");
+  const trackNameEle = createEle("span", "track__name");
+  const trackAudioEle = createEle("audio", "track__audio");
+  const isHotEle = createEle("img", "track__is-hot");
   if (data.isHot) {
-    isHot.src = "./icons/is-hot.png";
+    isHotEle.src = "./src/icons/is-hot.png";
   } else {
-    isHot.innerHTML = "";
+    isHotEle.innerHTML = "";
   }
 
-  trackEleImage.src = data.coverImageUrl;
-  trackArtistName.innerHTML = `${data.artistName} - `;
-  trackName.innerHTML = `${data.title}`;
-  trackAudio.controls = "controls";
-  trackAudio.src = data.fileUrl;
+  trackImageEle.src = data.coverImageUrl;
+  trackArtistNameEle.innerHTML = `${data.artistName} - `;
+  trackNameEle.innerHTML = `${data.title}`;
+  trackAudioEle.controls = true;
+  trackAudioEle.src = data.fileUrl;
 
-  trackTitle.append(isHot, trackArtistName, trackName);
-  trackInfo.append(trackTitle, trackAudio);
-  trackEle.append(trackEleImage, trackInfo);
+  trackTitleEle.append(isHotEle, trackArtistNameEle, trackNameEle);
+  trackInfoEle.append(trackTitleEle, trackAudioEle);
+  trackEle.append(trackImageEle, trackInfoEle);
   ele.append(trackEle);
 }
---
+++ src/components/data.module.js
@@ -0,0 +1,80 @@
+export const playlists = [
+  {
+    title: "Hip-Hop Hits",
+    coverImageUrl: "./src/img/hip-hop-hits.jpg",
+    info: {
+      totalTracksCount: 4,
+      totalTracksDurationInSeconds: 733,
+    },
+    label: "./src/icons/isHot.png",
+    tracks: [
+      {
+        coverImageUrl: "./src/img/eminem.png",
+        artistName: "Eminem",
+        title: "Rap God",
+        fileUrl: "./src/audio/eminem-rap-god.mp3",
+        isHot: true,
+      },
+      {
+        coverImageUrl: "./src/img/50cent.png",
+        artistName: "50cent",
+        title: "In da Club",
+        fileUrl: "./src/audio/50сent-in-da-club.mp3",
+        isHot: false,
+      },
+      {
+        coverImageUrl: "./src/img/dmx.png",
+        artistName: "DMX",
+        title: "X Gon' Give It To Ya",
+        fileUrl: "./src/audio/dmx-x-gon-give-it-to-ya.mp3",
+        isHot: true,
+      },
+      {
+        coverImageUrl: "./src/img/eminem50cent.png",
+        artistName: "Eminem feat 50 Cent, Cashis, Lloyd Banks",
+        title: "You Don't Know",
+        fileUrl: "./src/audio/eminem-50cent-you-dont-know.mp3",
+        isHot: true,
+      },
+    ],
+  },
+  {
+    title: "Rap Hits 1990s",
+    coverImageUrl: "./src/img/Rap-Hits-1990s.png",
+    info: {
+      totalTracksCount: 4,
+      totalTracksDurationInSeconds: 942,
+    },
+    label: "./src/icons/isHot.png",
+    tracks: [
+      {
+        coverImageUrl: "./src/img/public-enemy.png",
+        artistName: "Public Enemy",
+        title: "Fight the Power",
+        fileUrl: "./src/audio/public-enemy-fight-the-power.mp3",
+        isHot: true,
+      },
+      {
+        coverImageUrl: "./src/img/vanila-ice.png",
+        artistName: "Vanila Ice",
+        title: "Ice Ice Baby",
+        fileUrl: "./src/audio/vanilla-ice-ice-ice-baby.mp3",
+        isHot: true,
+      },
+      {
+        coverImageUrl: "./src/img/mc-hammer.png",
+        artistName: "MC Hammer",
+        title: "You Can’t Touch This",
+        fileUrl: "./src/audio/mc-hammer-u-can't-touch-this.mp3",
+        isHot: false,
+      },
+      {
+        coverImageUrl: "./src/img/public-enemy.png",
+        artistName: "Brand Nubian",
+        title: "Brand Nubian",
+        fileUrl: "./src/audio/brand-nubian-brand-nubian.mp3",
+        isHot: false,
+      },
+    ],
+  },
+];
