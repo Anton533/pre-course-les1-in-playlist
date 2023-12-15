390f52acc4bffbc1d1b213851e852f957d231410
Anton <anton.omelianenko533@gmail.com>
2023-12-15 19:48:15 +0200

    refactor: move main.js file

--- player.js
+++
@@ -1,4 +0,0 @@
-import { playlists } from "./src/components/data.component.js";
-import { renderPlaylists } from "./src/components/renderPlaylists.component.js";
-
-renderPlaylists(playlists);
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { playlists } from "./components/data.component.js";
+import { renderPlaylists } from "./components/renderPlaylists.component.js";
+
+renderPlaylists(playlists);
